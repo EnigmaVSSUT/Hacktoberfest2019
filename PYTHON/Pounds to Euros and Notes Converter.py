fdb88ed49108b9d95820d9e71f3f9389759d053e
sharion64 <55957877+sharion64@users.noreply.github.com>
2019-10-04 22:05:12 +0100

    Add files via upload

---
+++ PYTHON/Pounds to Euros and Notes Converter.py
@@ -0,0 +1,17 @@
+holidaypounds = int(input("Enter how many pounds you want to take on holiday: £"))
+euro = holidaypounds * 1.14
+print("You will have €", euro)
+
+pounds = holidaypounds / 1.14 #pound variable
+FiftyEuros = holidaypounds / 50 #variable for fifty euros
+TwentyEuros = holidaypounds / 20 #variable for twenty euros
+TenEuros = holidaypounds / 10 #variable for ten euros
+FiveEuros = holidaypounds / 5 #variable for five euros
+
+print("You will have €", FiftyEuros, "fifty note(s)", ) 
+print("You will have €", TwentyEuros, "twenty note(s)", )
+print("You will have €", TenEuros, "ten note(s)", )
+print("You will have €", FiveEuros, "five note(s)", )
+
+
+
