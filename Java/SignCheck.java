4ac2f2570eaa6d79e4425c4fb5e6398735b36538
RISHAV <rishavm7@gmail.com>
2019-10-13 02:26:22 +0530

    Added my code snippet

---
+++ Java/SignCheck.java
@@ -0,0 +1,28 @@
+import java.io.*;
+//This program checks if two numbers have the same sign or opposite sign.
+class program
+{ 
+    public static void main(String args[])throws IOException
+    {
+        InputStreamReader in=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(in);
+        System.out.println("Enter numbers");
+        int max,min;
+        int n1=Integer.parseInt(br.readLine());
+        int n2=Integer.parseInt(br.readLine());
+        if(n1>n2)
+        {
+             max=n1;
+             min=n2;
+        }
+        else
+        {
+            max=n2;
+            min=n1;
+        }
+        if(min<0 && max>0)
+            System.out.println("No");
+        else 
+            System.out.println("Yes");
+    }
+}
